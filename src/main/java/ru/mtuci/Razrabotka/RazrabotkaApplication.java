073bde5fd72bbb6a5e114286171954d1aef4fbb3
phoenix <gogulyok@gmail.com>
2024-10-23 11:29:03 +0300

    1

---
+++ src/main/java/ru/mtuci/Razrabotka/RazrabotkaApplication.java
@@ -0,0 +1,13 @@
+package ru.mtuci.Razrabotka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RazrabotkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RazrabotkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/mtuci/Razrabotka/RazrabotkaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mtuci.Razrabotka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RazrabotkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
